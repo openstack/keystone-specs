c4982df3920e9ecc82245902b159c3afcbbd39de
Doug Hellmann <doug@doughellmann.com>
2014-09-10 14:37:58 -0400

    Add RSS feed
    Publish an RSS feed of the changes to the specs repository to make the
    specs more discoverable.
    
    Change-Id: Ib4f84efab8b3af3ddd47d38d63a91362612bd655

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,8 +29,13 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
               'sphinx.ext.viewcode',
-              'oslosphinx'
+              'oslosphinx',
+              'yasfb',
              ]
+
+# Feed configuration for yasfb
+feed_base_url = 'http://specs.openstack.org/openstack/keystone-specs'
+feed_author = 'OpenStack Identity Team'
 
 todo_include_todos = True
 
